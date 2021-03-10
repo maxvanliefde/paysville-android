ae44bb1e031e4bb1e0614deed93305ba6c261398
maxvanliefde <54867093+maxvanliefde@users.noreply.github.com>
2021-03-11 00:18:03 +0100

    nouveaux packages model et controler pour implémenter l'architecture MVC

--- app/src/main/java/be/thefluffypangolin/paysville/controller/MainActivity.java
+++ app/src/main/java/be/thefluffypangolin/paysville/controller/MainActivity.java
@@ -1,8 +1,10 @@
-package be.thefluffypangolin.paysville;
+package be.thefluffypangolin.paysville.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import be.thefluffypangolin.paysville.R;
 
 public class MainActivity extends AppCompatActivity {
 
