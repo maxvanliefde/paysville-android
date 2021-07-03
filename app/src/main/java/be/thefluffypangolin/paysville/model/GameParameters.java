a083541b3c38100b96b4a57b7a5aeaaacacec0ca
maxvanliefde <54867093+maxvanliefde@users.noreply.github.com>
2021-07-03 22:55:02 +0200

    renomme Parameters en GameParameters et ajoute un toString basique

--- app/src/main/java/be/thefluffypangolin/paysville/model/GameParameters.java
+++ app/src/main/java/be/thefluffypangolin/paysville/model/GameParameters.java
@@ -1,7 +1,14 @@
 package be.thefluffypangolin.paysville.model;
 
+import android.content.SharedPreferences;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.jetbrains.annotations.NotNull;
+
 /**
- * La classe Parameters gère les paramètres d'une partie de Pays-Ville.
+ * La classe GameParameters gère les paramètres d'une partie de Pays-Ville.
  * Les paramètres sont : <br>
  * <ul>
  *     <li>Partie de 26 manches ou à points définis;</li>
@@ -9,7 +16,7 @@ package be.thefluffypangolin.paysville.model;
  *     <li><ul><li>Timer bonus pour les mauvaises lettres ou non;</li></ul></li>
  *     <li>Probabilité de doubler les points d'une manche;</li>
  */
-public class Parameters {
+public class GameParameters {
 
     public static class BadParameterException extends Exception {
         public BadParameterException(String param) {
@@ -48,7 +55,6 @@ public class Parameters {
     }
 
 
-
     public boolean isTimerOn() {
         return timerOn;
     }
@@ -65,7 +71,6 @@ public class Parameters {
         if (!timerOn) throw new BadParameterException("timerOn");
         this.timerDuration = timerDuration;
     }
-
 
 
     public boolean isBonusTimerOn() {
@@ -86,7 +91,6 @@ public class Parameters {
     }
 
 
-
     public boolean isRandomDoublePointsOn() {
         return randomDoublePointsOn;
     }
@@ -102,5 +106,43 @@ public class Parameters {
     public void setProbabilityRandom(int probabilityRandom) throws BadParameterException {
         if (!isRandomDoublePointsOn()) throw new BadParameterException("timerOn");
         this.probabilityRandom = probabilityRandom;
+    }
+
+    /**
+     * Retourne une instance de GameParameters paramétrée
+     * au moyen du SharedPreferences donné.
+     */
+    public GameParameters(SharedPreferences sharedPreferences) {
+        gameEndsWithPoints = sharedPreferences.getString("end_game_list", "").equals("fixed_points");
+        if (gameEndsWithPoints)
+            pointsGameEnd = Integer.parseInt(sharedPreferences.getString("end_game_points", "0"));
+
+        timerOn = sharedPreferences.getBoolean("timer_switch", false);
+        if (timerOn) {
+            timerDuration = Integer.parseInt(sharedPreferences.getString("timer_duration", "0"));
+
+            bonusTimerOn = sharedPreferences.getBoolean("dl_switch", false);
+            if (bonusTimerOn)
+                bonusTimerDuration = Integer.parseInt(sharedPreferences.getString("dl_time", "0"));
+        }
+
+        randomDoublePointsOn = sharedPreferences.getBoolean("rd_switch", false);
+        if (randomDoublePointsOn)
+            probabilityRandom = sharedPreferences.getInt("rd_probability", 20) / 20;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "GameParameters{" +
+                "gameEndsWithPoints=" + gameEndsWithPoints +
+                ", pointsGameEnd=" + pointsGameEnd +
+                ", timerOn=" + timerOn +
+                ", timerDuration=" + timerDuration +
+                ", bonusTimerOn=" + bonusTimerOn +
+                ", bonusTimerDuration=" + bonusTimerDuration +
+                ", randomDoublePointsOn=" + randomDoublePointsOn +
+                ", probabilityRandom=" + probabilityRandom +
+                '}';
     }
 }
