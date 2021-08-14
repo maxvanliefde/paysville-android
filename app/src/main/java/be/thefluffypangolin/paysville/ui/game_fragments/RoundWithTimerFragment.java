bdd133bc9624e10b2ffbc5a2975ffd3f78cb8a20
maxvanliefde <54867093+maxvanliefde@users.noreply.github.com>
2021-08-14 11:45:56 +0200

    début d'implémentation du timer todo améliorer le viewmodel avec un livedata

--- app/src/main/java/be/thefluffypangolin/paysville/ui/game_fragments/RoundWithTimerFragment.java
+++ app/src/main/java/be/thefluffypangolin/paysville/ui/game_fragments/RoundWithTimerFragment.java
@@ -8,17 +8,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 import org.jetbrains.annotations.NotNull;
 
 import be.thefluffypangolin.paysville.GameActivity;
 import be.thefluffypangolin.paysville.GameViewModel;
+import be.thefluffypangolin.paysville.R;
 import be.thefluffypangolin.paysville.databinding.FragmentGameRoundWithTimerBinding;
 import be.thefluffypangolin.paysville.model.PaysVilleGame;
 
@@ -29,8 +34,7 @@ public class RoundWithTimerFragment extends Fragment {
     private ExtendedFloatingActionButton fab;
     private FragmentGameRoundWithTimerBinding binding;
     private RoundWithTimerViewModel model;
-
-    // TODO : implement the timer
+    private LinearProgressIndicator progressBar;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -39,6 +43,28 @@ public class RoundWithTimerFragment extends Fragment {
         model = new ViewModelProvider(this).get(RoundWithTimerViewModel.class);
         fab = activity.getFAB();
         binding = FragmentGameRoundWithTimerBinding.inflate(inflater, container, false);
+        progressBar = binding.progressBarTimer;
+
+        // gestion du texte et du timer
+        if (!model.isTimerLaunched()){
+            setNewTimer();
+            setTextLetter();
+            fab.hide(new ExtendedFloatingActionButton.OnChangedCallback() {
+                @Override
+                public void onHidden(ExtendedFloatingActionButton extendedFab) {
+                    super.onHidden(extendedFab);
+                    customizeFAB();
+                }
+            });
+            launchTimer();
+        }
+        if (!model.isTimerFinished()){
+            setTextLetter();
+            customizeFAB();
+        } else {
+            setTextTimerFinished();
+            customizeFAB();
+        }
 
         return binding.getRoot();
     }
@@ -47,5 +73,47 @@ public class RoundWithTimerFragment extends Fragment {
     public void onAttach(@NonNull @NotNull Context context) {
         super.onAttach(context);
         activity = (GameActivity) context;
+    }
+
+    private void launchTimer() {
+        model.setTimerLaunchedTrue();
+        progressBar.setMax(game.getParameters().getTimerDuration());
+        progressBar.setProgressCompat(progressBar.getMax(), false);
+        model.getTimer().start();
+    }
+
+    private void customizeFAB() {
+        fab.setText(R.string.encode_points);
+        fab.setIconResource(R.drawable.ic_edit_24dp);
+        fab.setOnClickListener(v -> {
+            NavDirections action = RoundWithTimerFragmentDirections.actionRoundWithTimerToPoints();
+            Navigation.findNavController(activity, R.id.nav_host_fragment_activity_game).navigate(action);
+        });
+    }
+
+    private void setTextTimerFinished() {
+        binding.textAboveLetter.setText(R.string.text_round_times_up);
+        binding.roundLetter.setText("⏲");
+    }
+
+    private void setTextLetter() {
+        binding.textAboveLetter.setText(R.string.text_round_letter_is);
+        binding.roundLetter.setText(Character.toString(game.getCurrentRound().getLetter()));
+    }
+
+    private void setNewTimer() {
+        model.setTimer(new CountDownTimer(game.getParameters().getTimerDuration() * 1000L, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progressBar.setProgressCompat((int) millisUntilFinished / 1000, true);
+            }
+
+            @Override
+            public void onFinish() {
+                model.setTimerFinishedTrue();
+                setTextTimerFinished();
+                fab.show();
+            }
+        });
     }
 }
--- app/src/main/java/be/thefluffypangolin/paysville/ui/game_fragments/RoundWithTimerViewModel.java
+++ app/src/main/java/be/thefluffypangolin/paysville/ui/game_fragments/RoundWithTimerViewModel.java
@@ -1,7 +1,35 @@
 package be.thefluffypangolin.paysville.ui.game_fragments;
 
+import android.os.CountDownTimer;
+
 import androidx.lifecycle.ViewModel;
 
 public class RoundWithTimerViewModel extends ViewModel {
-    // TODO: Implement the ViewModel (timer)
+    private boolean timerLaunched = false;
+    private boolean timerFinished = false;
+    private CountDownTimer timer;
+
+    public boolean isTimerLaunched() {
+        return timerLaunched;
+    }
+
+    public void setTimerLaunchedTrue() {
+        this.timerLaunched = true;
+    }
+
+    public CountDownTimer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(CountDownTimer timer) {
+        this.timer = timer;
+    }
+
+    public boolean isTimerFinished() {
+        return timerFinished;
+    }
+
+    public void setTimerFinishedTrue() {
+        timerFinished = true;
+    }
 }
