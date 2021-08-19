adc682183ef61bc125faf74d94f297bba8eff972
maxvanliefde <54867093+maxvanliefde@users.noreply.github.com>
2021-08-19 18:08:03 +0200

    Convert AboutFragment to Kotlin and remove AboutViewModel

---
+++ app/src/main/java/be/thefluffypangolin/paysville/ui/main_fragments/AboutFragment.kt
@@ -0,0 +1,25 @@
+package be.thefluffypangolin.paysville.ui.main_fragments
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import be.thefluffypangolin.paysville.R
+import be.thefluffypangolin.paysville.databinding.FragmentMainAboutBinding
+
+class AboutFragment : Fragment() {
+    private lateinit var binding: FragmentMainAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainAboutBinding.inflate(inflater, container, false)
+        binding.textAbout.setText(R.string.title_about)
+        return binding.root
+    }
+}
