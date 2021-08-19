7d8fd405d31f12ab006e13ae85297711e55a8326
maxvanliefde <54867093+maxvanliefde@users.noreply.github.com>
2021-08-19 22:01:08 +0200

    Convert Player to Kotlin - one line :)

--- app/src/main/java/be/thefluffypangolin/paysville/model/Player.kt
+++ app/src/main/java/be/thefluffypangolin/paysville/model/Player.kt
@@ -1,28 +1,6 @@
-package be.thefluffypangolin.paysville.model;
-
-import org.jetbrains.annotations.NotNull;
+package be.thefluffypangolin.paysville.model
 
 /**
- * Cette classe représente un jour, désigné par son nom.
+ * Cette classe représente un joueur, désigné par son nom.
  */
-public class Player {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Player(String name) {
-        this.name = name;
-    }
-
-    @NotNull
-    @Override
-    public String toString() {
-        return name;
-    }
-}
+data class Player(val name: String)
